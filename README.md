2bcc5b82d5c89dbeea811c8e34b548f231be2dcb
Michael Thomas <michat@devolute.org>
2013-01-23 19:32:51 +0100

    add readme and seeds

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-backbone-moviedb
+Backbone MovieDB
 ================
 
-A Backbone/Rails Example App
+This is a minimal example app to demonstrate how use Backbone.js with Rails.
+It implements a small movie database, with basic CRUD interface.
+Further it includes a simple rating feature, user access control and faceted filter over
+categories and ratings.
+
+
+Setup
+-----
+
+The basic Rails setup:
+
+```
+bundle
+rake db:migrate
+rake db:seed
+```
+
+
+Login
+-----
+
+Use these credentials:
+
+    email: movie@devolute.org
+    password: movie123
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+User.delete_all
+User.create!(email: "movie@devolute.org", password: "movie123" )
+
+
+[
+  {title: "Fight Club", category: "Thriller", description: "How much can you know about yourself.."},
+  {title: "The Matrix", category: "Sci-Fi", description: "How deep does the rabbit hole go"},
+  {title: "Django Unchained", category: "Western", description: "The new Tarantino"},
+  {title: "Pi", category: "Thriller", description: "Mathematics is the language of nature"},
+  {title: "Departed", category: "Thriller", description: "Martin Scorsese"}
+].each do |movie|
+  Movie.create! movie
+end
